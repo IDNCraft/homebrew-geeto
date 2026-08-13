22b792fe698915ebd5f9866b9b53f479efc2d871
Agung Maulana <amdev142@gmail.com>
2026-08-13 14:40:53 +0700

    Update Geeto formula to new GitHub repository

--- Formula/geeto.rb
+++ Formula/geeto.rb
@@ -3,20 +3,20 @@
 
 class Geeto < Formula
   desc "AI-powered Git workflow automation CLI"
-  homepage "https://github.com/rust142/geeto"
+  homepage "https://github.com/IDNCraft/geeto"
   version "0.13.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/rust142/geeto/releases/download/v0.13.0/geeto-mac"
+      url "https://github.com/IDNCraft/geeto/releases/download/v0.13.0/geeto-mac"
       sha256 "9d16e5966a64a91dae2637bc9354da389c661387150029f5f687df6a7a243eb5"
 
       def install
         bin.install "geeto-mac" => "geeto"
       end
     elsif Hardware::CPU.arm?
-      url "https://github.com/rust142/geeto/releases/download/v0.13.0/geeto-mac-arm64"
+      url "https://github.com/IDNCraft/geeto/releases/download/v0.13.0/geeto-mac-arm64"
       sha256 "2e5cbec4f6406c0435456f628992279438e48f0ab98acc9bafceffab31b75711"
 
       def install
@@ -27,14 +27,14 @@ class Geeto < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/rust142/geeto/releases/download/v0.13.0/geeto-linux"
+      url "https://github.com/IDNCraft/geeto/releases/download/v0.13.0/geeto-linux"
       sha256 "ac0c5def47563ffad3f6d2a8ab40d37fca6a7de2b0d6c3e43dce69f079ba0704"
 
       def install
         bin.install "geeto-linux" => "geeto"
       end
     elsif Hardware::CPU.arm?
-      url "https://github.com/rust142/geeto/releases/download/v0.13.0/geeto-linux-arm64"
+      url "https://github.com/IDNCraft/geeto/releases/download/v0.13.0/geeto-linux-arm64"
       sha256 "5625822cad822818149c92cdc76d552d2db74c47a3f62730f07c2835917af3a8"
 
       def install
